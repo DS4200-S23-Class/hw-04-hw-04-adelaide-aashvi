f4bc84c7b0e71a22e831bc6ca44ca1db48e724c4
Adelaide <bsharah.a@northeastern.edu>
2023-02-14 18:42:16 -0500

    push 4

--- hw_04.js
+++ hw_04.js
@@ -1,10 +1,11 @@
 // give border when clicked
     function addBorder(id, x1, x2) {
-        if (document.getElementById(id).style.stroke == "lightblue" && document.getElementById(id).strokeWidth == 5) {
+        if (document.getElementById(id).style.stroke == "cyan") {
             document.getElementById(id).style.stroke = "none";
         }
 
         else {
-            document.getElementById(id).style.stroke ="lightblue";
+            document.getElementById(id).style.stroke ="cyan";
+            document.getElementById(id).setAttribute("stroke-width", "3px");
         }
     };
